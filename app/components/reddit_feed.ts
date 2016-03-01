8a0f5f158f9a0d113f997c9a0fa868429cb699f1
Benny Ng <philoman@gmail.com>
2016-03-02 00:18:44 +0800

    add logger and refactor

--- app/components/reddit_feed.ts
+++ app/components/reddit_feed.ts
@@ -11,23 +11,27 @@ import {ImageReddit} from './reddit_feed.models';
 })
 export class RedditFeed {
 
-  private _reddits: Observable<Array<ImageReddit>>;
+  private _reddits$: Observable<Array<ImageReddit>>;
   private _redditDataUrl: string = 'http://www.reddit.com/r/9gag.json';
 
   constructor(private _http: Http) {
   }
 
   ngOnInit() {
-    this._fetchReddits();
+    this._initFeed();
+    this._logFeed();
   }
 
-  private _fetchReddits() {
-    this._reddits = this._http.get(this._redditDataUrl)
+  private _initFeed() {
+    this._reddits$ = this._http.get(this._redditDataUrl)
       .map(response => response.json())
       .map(json => <Array<any>>json.data.children)
       .map(children => children.filter(d => (
         ['png', 'jpg'].indexOf(d.data.url.split('.').pop()) != -1
       )))
       .map(children => children.map(d => new ImageReddit(d.data.id, d.data.title, d.data.url)));
+  }
+  private _logFeed() {
+    this._reddits$.subscribe(data => console.debug('data', data));
   }
 }
